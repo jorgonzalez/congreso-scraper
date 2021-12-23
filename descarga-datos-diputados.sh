81903248af9c3457a8defad5ca7175ce9342c099
jorge.gonzalez <jorge.gonzalez@endocode.com>
2021-12-23 21:21:21 +0100

    Version inicial (y guarrisima) del scraper

---
+++ descarga-datos-diputados.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+TOTAL_DIPUTADOS=388
+LEGISLATURA="XIV"
+FICHERO="datos-personales-diputados.csv"
+
+rm "${FICHERO}" 2>/dev/null
+
+echo "ID_DIPUTADO;NOMBRE;PROVINCIA;GRUPO;TWITTER;FACEBOOK;CORREO" >> "${FICHERO}"
+
+for ID_DIPUTADO in $(seq 1 ${TOTAL_DIPUTADOS}); do
+	#Limpiar variables que puede que noexistan
+	TWITTER=""
+	FACEBOOK=""
+	CORREO=""
+
+	URL="https://www.congreso.es/busqueda-de-diputados?p_p_id=diputadomodule&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_diputadomodule_mostrarFicha=true&codParlamentario=${ID_DIPUTADO}&idLegislatura=${LEGISLATURA}&mostrarAgenda=false"
+
+	TEMPORAL=$(wget -qO - "${URL}")
+
+	NOMBRE=$(echo "${TEMPORAL}" | grep "nombre-dip" -A 1 | tail -n 1 | sed 's/\r$//' | sed -e 's/^[ \t]*//')
+	PROVINCIA=$(echo "${TEMPORAL}" | grep "cargo-dip" -A 1 | tail -n 1 | sed 's/\r$//' | sed -e 's/^[ \t]*//')
+	GRUPO=$(echo "${TEMPORAL}" | grep "grupo-dip" -A 3 | tail -n 1 | sed 's/\r$//' | sed -e 's/^[ \t]*//')
+	TWITTER=$(echo "${TEMPORAL}" | grep -m 1 "twitter.com" | awk -F"=" '{print $2}' | awk '{print $1}' | tr -d "\"" | sed -e 's/^[ \t]*//')
+	FACEBOOK=$(echo "${TEMPORAL}" | grep -m 1 "facebook.com" | awk -F"=" '{print $2}' | awk '{print $1}' | tr -d "\"" | sed -e 's/^[ \t]*//')
+	CORREO=$(echo "${TEMPORAL}" | grep "mailto:" | awk -F"=" '{print $4}' | awk -F":" '{print $2}' | awk -F">" '{print $1}' | tr -d "\"" | sed -e 's/^[ \t]*//')
+
+	if [[ "${TWITTER}" == "https://twitter.com/Congreso_Es" ]]; then
+		TWITTER=""
+	fi
+	if [[ "${FACEBOOK}" == "https://www.facebook.com/CongresodelosDiputados" ]]; then
+		FACEBOOK=""
+	fi
+
+	echo "${ID_DIPUTADO};${NOMBRE};${PROVINCIA};${GRUPO};${TWITTER};${FACEBOOK};${CORREO}" >> "${FICHERO}"
+done
